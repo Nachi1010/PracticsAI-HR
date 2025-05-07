05b80943b89c778c5e7586cf1f52619ae12592f3
Your Name <user@example.com>
2025-05-07 04:47:45 +0300

    שינוי לסאבדומיין hr.practicsai.com והסרת הגדרות של נתיב משנה

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <UserDataProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/hr">
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
